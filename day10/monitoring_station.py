f8553dfbc367401d901adc6f099e427ba116e23d
JJ Graham <thetwoj@gmail.com>
2019-12-11 20:24:39 -0600

    adding solution for day 10 part 1

--- day10/monitoring_station.py
+++ day10/monitoring_station.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import math
 
 
 def read_map():
@@ -35,121 +36,130 @@ def find_asteroids(sm):
     return asteroids
 
 
-def next_move(last_move, x, y, max_x, max_y, explored):
-    if last_move == 0:  # L
-        if (x, y - 1) not in explored and y - 1 >= 0:
-            return x, y - 1, 1
-        if (x - 1, y) not in explored and x - 1 >= 0:
-            return x - 1, y, 0
-        if (x, y + 1) not in explored and y + 1 < max_y:
-            return x, y + 1, 3
-        if (x + 1, y) not in explored and x + 1 < max_x:
-            return x + 1, y, 2
-    if last_move == 1:  # U
-        if (x + 1, y) not in explored and x + 1 < max_x:
-            return x + 1, y, 2
-        if (x, y - 1) not in explored and y - 1 >= 0:
-            return x, y - 1, 1
-        if (x - 1, y) not in explored and x - 1 >= 0:
-            return x - 1, y, 0
-        if (x, y + 1) not in explored and y + 1 < max_y:
-            return x, y + 1, 3
-    if last_move == 2:  # R
-        if (x, y + 1) not in explored and y + 1 < max_y:
-            return x, y + 1, 3
-        if (x + 1, y) not in explored and x + 1 < max_x:
-            return x + 1, y, 2
-        if (x, y - 1) not in explored and y - 1 >= 0:
-            return x, y - 1, 1
-        if (x - 1, y) not in explored and x - 1 >= 0:
-            return x - 1, y, 0
-    if last_move == 3:  # D
-        if (x - 1, y) not in explored and x - 1 >= 0:
-            return x - 1, y, 0
-        if (x, y + 1) not in explored and y + 1 < max_y:
-            return x, y + 1, 3
-        if (x + 1, y) not in explored and x + 1 < max_x:
-            return x + 1, y, 2
-        if (x, y - 1) not in explored and y - 1 >= 0:
-            return x, y - 1, 1
-
-
-def count_viewable_asteroids(sm, asteroid):
-    # print(asteroid)
+def count_viewable_asteroids(sm, station, asteroids, print_map=False):
     blocked_coords = set()
-    blocked_coords.add(asteroid)
+    blocked_coords.add(station)
     sights = set()
     visible_count = 0
-    og_x, og_y = asteroid
-    x, y = og_x, og_y
+    og_x, og_y = station
     max_x = len(sm)
     max_y = len(sm[0])
 
-    last_move = 0
-    explored = []
-    explored.append((x, y))
-    while len(explored) < max_x * max_y:
-        x, y, last_move = next_move(
-            last_move, x, y, max_x, max_y, explored
-        )  # 0:L, 1:U, 2:R, 3:D
-        explored.append((x, y))
-        print(explored)
+    distances = {}
+    for a_x, a_y in asteroids:
+        distances[(a_x, a_y)] = abs(og_x - a_x) + abs(og_y - a_y)
+    distances = sorted(distances, key=distances.get)
 
+    for x, y in distances:
         if (x, y) in blocked_coords or sm[x][y] != "#":
             continue
         visible_count += 1
         sights.add((x, y))
-        blocked_coords.add((x, y))
+        # blocked_coords.add((x, y))
 
         if og_x == x:
             if y < og_y:
                 for new_y in range(0, og_y + 1):
-                    blocked_coords.add((x, new_y))
+                    if sm[x][new_y] == "#":
+                        blocked_coords.add((x, new_y))
                 continue
             else:
-                for new_y in range(og_y, max_y + 1):
-                    blocked_coords.add((x, new_y))
+                for new_y in range(og_y, max_y):
+                    if sm[x][new_y] == "#":
+                        blocked_coords.add((x, new_y))
                 continue
 
         if og_y == y:
             if x < og_x:
                 for new_x in range(0, og_x + 1):
-                    blocked_coords.add((new_x, y))
+                    if sm[new_x][y] == "#":
+                        blocked_coords.add((new_x, y))
                 continue
             else:
-                for new_x in range(og_x, max_x + 1):
-                    blocked_coords.add((new_x, y))
+                for new_x in range(og_x, max_x):
+                    if sm[new_x][y] == "#":
+                        blocked_coords.add((new_x, y))
                 continue
 
+        gcd = math.gcd(abs(x - og_x), abs(y - og_y))
         if x > og_x and y > og_y:
-            new_x = x + (x - og_x)
-            new_y = y + (y - og_x)
-            while max_x - 1 > new_x > og_x and max_y - 1 > new_y > og_y:
-                new_x = new_x + (x - og_x)
-                new_y = new_y + (y - og_x)
+            new_x = x + (x - og_x) // gcd
+            new_y = y + (y - og_y) // gcd
+            while max_x > new_x > og_x and max_y > new_y > og_y:
+                blocked_coords.add((new_x, new_y))
+                new_x = new_x + (x - og_x) // gcd
+                new_y = new_y + (y - og_y) // gcd
+            new_x = x - (x - og_x) // gcd
+            new_y = y - (y - og_y) // gcd
+            while max_x > new_x > og_x and max_y > new_y > og_y:
+                blocked_coords.add((new_x, new_y))
+                new_x = new_x - (x - og_x) // gcd
+                new_y = new_y - (y - og_y) // gcd
 
-    print(asteroid)
-    new_map = deepcopy(sm)
-    for x, y in blocked_coords:
-        new_map[x][y] = "x"
-    for x, y in sights:
-        new_map[x][y] = "O"
-    new_map[og_x][og_y] = "#"
-    print()
-    print_star_map(new_map)
-    print()
-    print(visible_count)
+        if x > og_x and y < og_y:
+            new_x = x - (og_x - x) // gcd
+            new_y = y - (og_y - y) // gcd
+            while max_x > new_x > og_x and og_y > new_y >= 0:
+                blocked_coords.add((new_x, new_y))
+                new_x = new_x - (og_x - x) // gcd
+                new_y = new_y - (og_y - y) // gcd
+            new_x = x + (og_x - x) // gcd
+            new_y = y + (og_y - y) // gcd
+            while max_x > new_x > og_x and og_y > new_y >= 0:
+                blocked_coords.add((new_x, new_y))
+                new_x = new_x + (og_x - x) // gcd
+                new_y = new_y + (og_y - y) // gcd
+
+        if x < og_x and y > og_y:
+            new_x = x - (og_x - x) // gcd
+            new_y = y - (og_y - y) // gcd
+            while og_x > new_x >= 0 and max_y > new_y > og_y:
+                blocked_coords.add((new_x, new_y))
+                new_x = new_x - (og_x - x) // gcd
+                new_y = new_y - (og_y - y) // gcd
+            new_x = x + (og_x - x) // gcd
+            new_y = y + (og_y - y) // gcd
+            while og_x > new_x >= 0 and max_y > new_y > og_y:
+                blocked_coords.add((new_x, new_y))
+                new_x = new_x + (og_x - x) // gcd
+                new_y = new_y + (og_y - y) // gcd
+
+        if x < og_x and y < og_y:
+            new_x = x + (x - og_x) // gcd
+            new_y = y + (y - og_y) // gcd
+            while og_x > new_x >= 0 and og_y > new_y >= 0:
+                blocked_coords.add((new_x, new_y))
+                new_x = new_x + (x - og_x) // gcd
+                new_y = new_y + (y - og_y) // gcd
+            new_x = x - (x - og_x) // gcd
+            new_y = y - (y - og_y) // gcd
+            while og_x > new_x >= 0 and og_y > new_y >= 0:
+                blocked_coords.add((new_x, new_y))
+                new_x = new_x - (x - og_x) // gcd
+                new_y = new_y - (y - og_y) // gcd
+
+    if print_map:
+        new_map = deepcopy(sm)
+        for x, y in blocked_coords:
+            new_map[x][y] = "x"
+        for x, y in sights:
+            new_map[x][y] = "O"
+        new_map[og_x][og_y] = "#"
+        print_star_map(new_map)
     return visible_count
 
 
 if __name__ == "__main__":
     star_map = read_map()
-    print_star_map(star_map)
+    # print_star_map(star_map)
     asteroids = find_asteroids(star_map)
     results = {}
     for asteroid in asteroids:
-        results[asteroid] = count_viewable_asteroids(star_map, asteroid)
-    print()
+        results[asteroid] = count_viewable_asteroids(star_map, asteroid, asteroids)
+    # print()
     max_asteroid = max(results, key=results.get)
-    print(max_asteroid)
-    print(results[max_asteroid])
+    print("part one:")
+    count_viewable_asteroids(star_map, max_asteroid, asteroids, print_map=True)
+    print(f"station coords: {max_asteroid}")
+    viewable_asteroid_count = results[max_asteroid]
+    print(f"visible asteroids: {viewable_asteroid_count}")
